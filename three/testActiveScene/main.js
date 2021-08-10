8798e24381cdaac8775e3897e164d5b23eec6533
Imagineham <hamletfernandez57@gmail.com>
2021-08-10 00:53:54 -0400

    implemented scene switching and changing composer using gui

--- three/testActiveScene/main.js
+++ three/testActiveScene/main.js
@@ -3,12 +3,24 @@ import {EffectComposer} from 'https://threejsfundamentals.org/threejs/resources/
 import {RenderPass} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/postprocessing/RenderPass.js';
 import {ShaderPass} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/postprocessing/ShaderPass.js';
 import {FilmPass} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/postprocessing/FilmPass.js';
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
 
-
-let scene, rtScene, camera, rtCamera, renderer, canvas;
+let scene, rtScene, camera, rtCamera, renderer, canvas, scenes = [];
 let filmRenderTarget, finalRenderTarget;
 let cube, rtCubes;
-let filmComposer, finalComposer;
+let filmComposer, finalComposer, filmPass;
+let controls;
+
+//RAYCASTING VAR
+let raycaster, INTERSECTED, CLICK;
+let mouse = {
+  x: undefined,
+  y: undefined
+}
+
+//dat.gui
+let gui, folder1;
 
 init();
 animate();
@@ -20,6 +32,12 @@ function init() {
   renderer.setPixelRatio(devicePixelRatio);
   document.body.appendChild(canvas);
   window.addEventListener( 'resize', onWindowResize );
+
+  //raycaster
+  raycaster = new THREE.Raycaster();
+
+  //dat.gui
+
 
   const rtWidth = 512;
   const rtHeight = 512;
@@ -82,6 +100,9 @@ function init() {
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.z = 3;
 
+  //Orbit Controls
+  controls = new OrbitControls(camera, canvas);
+
   scene = new THREE.Scene();
 
   {
@@ -103,22 +124,24 @@ function init() {
   const cube2= new THREE.Mesh(geometry, material2);
   cube2.position.set(1, 0, -2)
   scene.add(cube);
+  scenes.push(cube);
   scene.add(cube2);
 
   filmComposer = new EffectComposer(renderer, filmRenderTarget);
   filmComposer.addPass(new RenderPass(rtScene, rtCamera));
-	const filmPass = new FilmPass(
+	filmPass = new FilmPass(
 		1,   // noise intensity
 		0.025,  // scanline intensity
 		648,    // scanline count
 		true,  // grayscale
 	);
-	//filmPass.enabled = false;
+	filmPass.enabled = false;
   //filmPass.renderToScreen = true;
   //filmPass.needsSwap = true;
 	filmComposer.addPass(filmPass);
-  filmComposer.setSize(rtWidth, rtHeight);
+  filmComposer.setSize(innerWidth, innerHeight);
 
+    /*
     //we need this shader to blend the the film scene and the mains scene together
     let finalshader = {
       uniforms: {
@@ -154,12 +177,13 @@ function init() {
     };
   
     finalshader.uniforms[ "tFilm" ].value = filmComposer.renderTarget2;
-
+    */
     const finalScene = new RenderPass(scene, camera);
-
+    /*
     let finalPass = new ShaderPass(finalshader);
     finalPass.needsSwap = true;
     finalPass.renderToScreen = true;
+    */
     finalComposer = new EffectComposer(renderer, finalRenderTarget);
  
     finalComposer.addPass(finalScene);
@@ -170,18 +194,29 @@ function init() {
 
 
 
-
 }
 
 function render() {
   //renderer.render(scene, camera);
+  controls.object = camera;
+  filmPass.enabled = true;
   filmComposer.render();
   finalComposer.render();
+  CLICK = false;
 }
 
 function animate() {
   requestAnimationFrame(animate);
-  render();
+
+  if(!CLICK) {
+    //console.log(CLICK);
+    render();
+  } else {
+    controls.object = rtCamera;
+    filmComposer.render();
+
+  }
+
 
   // rotate all the cubes in the render target scene
   rtCubes.forEach((cube) => {
@@ -198,6 +233,26 @@ function animate() {
   // rotate the cube in the scene
   cube.rotation.x += 0.001;
   cube.rotation.y += 0.001;
+
+
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(scenes);
+
+  if(intersects.length > 0) {
+    if (INTERSECTED != intersects[0].object) {
+      INTERSECTED = intersects[0].object;
+      INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+      INTERSECTED.material.emissive.setHex(0xafafaf);
+    }
+    
+  } else {
+    if (INTERSECTED) {
+      INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+      INTERSECTED = null;
+    }
+  }
+
+
 }
 
 function onWindowResize() {
@@ -205,5 +260,30 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
+
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1;
+})
+
+addEventListener('click', () => {
+    if(INTERSECTED && !CLICK) {
+      gui = new dat.GUI();
+      folder1 = gui.addFolder('FilmPass');
+      folder1.add(filmPass, 'enabled');
+      CLICK = true;
+    } else {
+      //CLICK = false;
+    }
+})
+
+addEventListener('keydown', (e) => {
+  if (e.keyCode === 27) {
+    gui.destroy();
+    console.log(e.keyCode);
+    render();
+  }
+})
+
+
